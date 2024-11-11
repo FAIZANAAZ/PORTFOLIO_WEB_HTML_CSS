92c9e2dbf05412fb4da8153ea555bea0eeb78d73
FAIZANAAZ779 <faixanaaz@gmail.com>
2024-11-11 21:05:31 +0500

    first commit

---
+++ script.ts
@@ -0,0 +1,107 @@
+// turn page wehn clink on next and prev button
+
+const pageturnbtn:any=document.querySelectorAll(".nextprev-btn")
+
+pageturnbtn.forEach((el:any,index:any) => {
+
+    el.onclick=()=>{
+        const pageturnid =el.getAttribute("data-page");
+        const pageturn =document.getElementById(pageturnid)
+    
+    if (pageturn.classList.contains("turn")) {
+        pageturn.classList.remove("turn")
+        setTimeout(()=>{
+            pageturn.style.zIndex=20-index
+        },500)
+        
+    }else{
+        pageturn.classList.add("turn")
+        setTimeout(()=>{
+            pageturn.style.zIndex=20+index
+        },500)
+    }
+
+}
+});
+
+// contact are button when click
+const pages =document.querySelectorAll(".book-page.page-right")
+const contactmebtn =document.querySelector(".btn.contact-me")
+
+contactmebtn.onclick=()=>{
+    pages.forEach((page:any,index:any)=>{
+        setTimeout(()=>{
+            page.classList.add("turn")
+
+setTimeout(()=>{
+    page.style.zIndex=20+index
+},500)
+
+        },(index+1)*200+100)
+
+
+    })
+
+}
+// create reverse index func
+
+let totalpage = pages.length
+let pagenumber =0
+
+function reverseindex() {
+    pagenumber--
+    if (pagenumber<0){
+        pagenumber= totalpage-1
+    }
+    
+}
+
+// back profile button when click
+const backprofilebtn =document.querySelector(".back-profile")
+backprofilebtn.onclick=()=>{
+    pages.forEach((_:any,index:any)=>{
+        setTimeout(()=>{
+            reverseindex()
+            pages[pagenumber].classList.remove("turn")
+
+            setTimeout(()=>{
+            reverseindex()
+
+                pages[pagenumber].style.zIndex=10+index
+
+            },500)
+        },(index+1)*200+100)
+    })
+}
+
+// opening animation book
+const  coverRight =document.querySelector(".cover.cover-right")
+const  pageleft =document.querySelector(".book-page.page-left")
+
+// opening animation (cover right animation)
+setTimeout(()=>{
+    coverRight.classList.add("turn")
+},2100)
+
+setTimeout(()=>{
+    coverRight.style.zIndex= -1
+},2800)
+// opening animation (cover right animation)
+
+setTimeout(()=>{
+    pageleft.style.zIndex=20
+},3200)
+// // opening all page left or profile page animation 
+pages.forEach((_:any,index:any)=>{
+    setTimeout(()=>{
+        reverseindex()
+        pages[pagenumber].classList.remove("turn")
+
+        setTimeout(()=>{
+        reverseindex()
+
+            pages[pagenumber].style.zIndex=10+index
+
+        },500)
+    },(index+1)*200+2100)
+})
---
+++ static_resume/static.js
@@ -0,0 +1,13 @@
+"use strict";
+document.getElementById("dawnload").addEventListener("click", () => {
+    const editdiv = document.getElementById("editdiv");
+    if (editdiv) {
+        editdiv.style.display = "none";
+    }
+    const printcontent = document.getElementById("maindiv").innerHTML;
+    const orgnalcontent = document.body.innerHTML;
+    document.body.innerHTML = printcontent;
+    window.print();
+    document.body.innerHTML = orgnalcontent;
+    window.location.reload();
+});
